41b6e5cb21ac7b0a994a7273cea885fa18088b91
Andrew Patipaksiri <apax714@gmail.com>
2017-03-28 20:08:47 -0700

    init project files

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <section id="rubik">
+          <div id="cube-container">
+            <div id="cube-viewport">
+              
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
